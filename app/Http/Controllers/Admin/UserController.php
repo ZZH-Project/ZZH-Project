29dbe2f058a252fb89138d3d354269ba16b8b661
hff <450292030@qq.com>
2017-03-24 16:21:40 +0800

    优化路由

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    //用户登录
+    public function login(){
+        return view('admin.login.adminLogin');
+    }
     //显示用户
     public function show() {
         return view('admin.user.userList');

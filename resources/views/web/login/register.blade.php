c528072887f2f2f8b93c2ad45f5af01cef89d18d
hff <450292030@qq.com>
2017-03-25 18:49:27 +0800

    1.实现验证码2.提示错误信息的修改

--- resources/views/web/login/register.blade.php
+++ resources/views/web/login/register.blade.php
@@ -6,6 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=0.5, minimum-scale=0.5, maximum-scale=0.5" />
 		<link href="{{asset('css/base.css')}}" rel="stylesheet" />
 		<link href="{{asset('css/web/login_zl.css')}}" rel="stylesheet" />
+		<link href="{{asset('css/web/login_hff.css')}}" rel="stylesheet" />
+{{--		<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />--}}
 		<script src="{{asset('js/jquery_2_1_4.min.js')}}" type="text/javascript"></script>
 		<script type="text/javascript">
 			$(function(){
@@ -19,24 +21,44 @@
 		</script>
 	</head>
 	<body>
+		{{--@if(count($errors)>0)--}}
+			{{--<div class="alert alert-danger">--}}
+				{{--<ul>--}}
+					{{--@foreach($errors->all() as $error)--}}
+						{{--<li>{{$error}}</li>--}}
+					{{--@endforeach--}}
+				{{--</ul>--}}
+			{{--</div>--}}
+		{{--@endif--}}
 		<div class="body">
 			<div class="login_logo"><img src="{{asset('images/web/login_logo.png')}}" /></div>
+			<form action="{{url('web/check')}}" method="post">
+				{{csrf_field()}}
 			<div class="login_wrap">
 				<ul class="login_ul">
 					<li>
 						<img src="{{asset('images/web/icon_phone.png')}}" class="login_icon" />
-						<input type="text" placeholder="请输入手机号" class="input_text" />
+						<input type="text" name="username" placeholder="请输入用户名" class="input_text" />
 					</li>
+					@if(count($errors)>0)
+						<div class="error-alert">{{$errors->first('username')}}</div>
+					@endif
 					<li>
 						<img src="{{asset('images/web/icon_password.png')}}" class="login_icon" />
-						<input type="text" placeholder="请输入6-20个字符的密码" class="input_text" />
+						<input type="text" name="password" placeholder="请输入6-20个字符的密码" class="input_text" />
 					</li>
+					@if(count($errors)>0)
+						<div class="error-alert">{{$errors->first('password')}}</div>
+					@endif
 					<li>
 						<img src="{{asset('images/web/icon_validate.png')}}" class="login_icon"  />
-						<input type="text" placeholder="请输入验证码" class="input_text" />
+						<input type="text" name="captcha" placeholder="请输入验证码" class="input_text" />
 						<img src="{{Captcha::src()}}" onclick="this.src=this.src+'?'+(new Date()).getTime()" class="validate_code"  />
 						{{--<a href="#" class="validate_code">获取验证码</a>--}}
 					</li>
+					@if(count($errors)>0)
+						<div class="error-alert">{{$errors->first('captcha')}}</div>
+					@endif
 					<li class="special login_tip">
 						<div class="checkbox_bar special">
 							<input type="checkbox" class="checkbox" id="checkbox" /><label for="checkbox">我已阅读并且同意<a href="#">《美丽网络服务使用协议》</a></label>
@@ -49,9 +71,10 @@
 					</li>
 				</ul>
 			</div><!--login_wrap-->
+			</form>
 			<a href="{{url('web/login')}}" class="a_register special">已有账号？立即登录</a>
 			<a href="#" class="btn_close"><img src="{{asset('images/web/icon_close.png')}}" /></a>
-			<div class="login_error_bar">手机号或密码错误</div>
+			{{--<div class="login_error_bar">手机号或密码错误</div>--}}
 		</div><!--body-->
 	</body>
 </html>

b8d3805decce4a9ddd99248808a0ea5bd0330d3b
xuhaha123 <496810314@qq.com>
2017-03-30 21:04:40 +0800

    后台权限管理的添加进行中（刚开始），加入无限级递归函数

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\PermissionRole;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class PermissionController extends Controller
 {
-    //
+    //显示权限
+    public function show() {
+        //获取数据
+        $fv = '';
+        $data = PermissionRole::get()->toArray();
+        return view('admin.permission.permissionList', ['data' => $data, 'fv' => $fv]);
+    }
+    //添加权限
+    public function add(Request $request) {
+        if ($request->isMethod('get')) {
+            return view('admin.permission.permissionAdd');
+        } elseif ($request->isMethod('post')) {
+
+        }
+    }
 }
--- resources/views/admin/permission/permissionList.blade.php
+++ resources/views/admin/permission/permissionList.blade.php
@@ -11,38 +11,35 @@
         <div style="padding:10px 0;">
             <div class="find">权限搜索：</div><input id="uf" class="myinput-main" type="text" value="{{$fv}}">
             <input class="token" type="hidden" name="_token" value="{{csrf_token()}}">
-            <a class="add" href="{{url('admin/user/add')}}"><i class="fa fa-user-plus" title="添加权限"></i></a>
+            <a class="add" href="{{url('admin/permission/add')}}"><i class="fa fa-plus" title="添加权限"></i></a>
             <div class="clear"></div>
         </div>
         <table class="tb">
             <tr>
                 <th style="border-left: 1px solid #3399ff;">ID</th>
-                <th>用户名</th>
-                <th>角色</th>
-                <th>邮箱</th>
-                <th style="border-right: 1px solid #3399ff;">用户操作</th>
+                <th>权限名称</th>
+                <th>权限路由</th>
+                <th>描述</th>
+                <th>是否为菜单</th>
+                <th style="border-right: 1px solid #3399ff;">权限操作</th>
             </tr>
             @foreach($data as $v)
                 <tr class="trd">
                     <td style="border-left: 1px solid #e5e5e5;">{{$v['id']}}</td>
-                    <td>{{$v['username']}}</td>
-                    <td>无</td>
-                    <td>{{$v['email'] != null ? $v['email'] : '无'}}</td>
+                    <td>{{$v['display_name']}}</td>
+                    <td>{{$v['name']}}</td>
+                    <td>{{$v['description']}}</td>
+                    <td>{{$v['is_menu'] == 0 ? '否' : '是'}}</td>
                     <td>
-                        <a class="active" href="{{url('admin/user/edit').'/'.$v['id']}}/{{isset($_GET['page']) ? $_GET['page'] : 1}}/{{$fv}}">
-                            <i class="fa fa-user-secret" title="修改信息"></i>
+                        <a class="active" href="{{url('admin/permission/edit').'/'.$v['id']}}/{{$fv}}">
+                            <i class="fa fa-user-secret" title="修改权限"></i>
                         </a>
-                        <a class="active" href="{{url('admin/user/del').'/'.$v['id']}}">
-                            <i class="fa fa-user-times" title="删除用户"></i>
+                        <a class="active" href="{{url('admin/permission/del').'/'.$v['id']}}">
+                            <i class="fa fa-user-times" title="删除权限"></i>
                         </a>
                     </td>
                 </tr>
             @endforeach
-            <tr>
-                <td colspan="5" style="border-left: 1px solid #e5e5e5;">
-                    {{$data->appends(['fv' => $fv])->links('public.zj_page')}}
-                </td>
-            </tr>
         </table>
     </div>
 @endsection

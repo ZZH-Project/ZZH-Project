99c887d9ac1e1f49fcfd9c8e9d9af9ec0b7420b9
xuhaha123 <496810314@qq.com>
2017-04-07 15:46:18 +0800

    专题首页随机专题完成

--- app/Http/Controllers/Web/ThemeController.php
+++ app/Http/Controllers/Web/ThemeController.php
@@ -14,9 +14,20 @@ class ThemeController extends Controller
 {
     //专题主页
     public function index(){
+        //获取评论次数
+        $count = ThemeComment::select(DB::raw('count(*) as num,th_id'))
+            ->groupBy('th_id')
+            ->get();
+        //查询随机专题
+        $list = ThemeList::select('theme_lists.*','theme_cates.cate_name')
+            ->leftjoin('theme_cates','theme_lists.cate_id','theme_cates.id')
+            ->where('theme_lists.is_show',1)
+            ->inRandomOrder()
+            ->limit(5)
+            ->get();
         //查询前5条专题分类
         $data = ThemeCate::orderBy('sort_id','asc')->limit(5)->get();
-        return view('web.theme.index', ['data' => $data]);
+        return view('web.theme.index', ['data' => $data,'count' => $count,'list' => $list]);
     }
     //专题分类列表
     public function show(){

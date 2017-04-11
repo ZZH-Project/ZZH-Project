d79422907c987a29f14ccf3c47afa6c95e58bca4
xuhaha123 <496810314@qq.com>
2017-04-11 19:20:27 +0800

    前台评论是否有昵称显示判断完成

--- resources/views/web/theme/details.blade.php
+++ resources/views/web/theme/details.blade.php
@@ -72,7 +72,11 @@
 							<img src="{{asset('images/web/user_img.png')}}" />
 							@endif
 						</div>
+						@if($v->wusername == null)
 						<span class="user_name">{{str_replace(substr($v->username,3,4),'****',$v->username)}}</span>
+						@else
+						<span class="user_name">{{$v->wusername}}</span>
+						@endif
 					</div>
 					<div class="right time_tip">{{$v->created_at}}</div>
 					<div style="clear: both;"></div>

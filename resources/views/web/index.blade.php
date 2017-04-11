762193b92bfc04fc3bee2b0af8eaa2b9288519e0
xuhaha123 <496810314@qq.com>
2017-04-11 09:42:00 +0800

    前台主页专题精选遍历完成

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -74,77 +74,39 @@
 
 	<div class="title_bar">
 		<div class="title_line"></div>
-		<h1 class="title_h3">最新精选</h1>
+		<h1 class="title_h3">精选</h1>
 	</div><!--title_bar-->
 
 	<div class="wrap">
-		<div class="theme_wrap">
-			<a href="./theme/details.html" class="theme_img_bar">
-				<img src="{{asset('images/web/theme_img.jpg')}}"  />
-				<div class="theme_cate_tip">美妆</div><!--theme_cate_tip-->
-			</a><!--theme_bar-->
-			<a href="./theme/details.html" class="title_h2">恋恋粉色季 画个甜甜的妆容吧！</a>
-			<div class="fun_info_bar">
-				<a href="javascript:void(0);" class="left">
-					<svg class="icon icon_em_25" aria-hidden="true">
-						<use xlink:href="#front_icon-pinglun"></use>
-				   </svg>
-					<span>20</span>
-				</a>
-				<a href="javascript:void(0);" class="left">
-					<svg class="icon icon_em_25" aria-hidden="true">
-						<use xlink:href="#front_icon-icondianzan"></use>
-					</svg>
-					<span>14</span>
-				</a>
-				<div style="clear: both;"></div>
-			</div><!--fun_info_bar-->
-		</div><!--theme_wrap-->
 
-		<div class="theme_wrap">
-			<a href="./theme/details.html" class="theme_img_bar">
-				<img src="{{asset('images/web/theme_img.jpg')}}"  />
-				<div class="theme_cate_tip">美妆</div><!--theme_cate_tip-->
-			</a><!--theme_bar-->
-			<a href="./theme/details.html" class="title_h2">恋恋粉色季 画个甜甜的妆容吧！</a>
-			<div class="fun_info_bar">
-				<a href="javascript:void(0);" class="left">
-					<svg class="icon icon_em_25" aria-hidden="true">
-						<use xlink:href="#front_icon-pinglun"></use>
-				   </svg>
-					<span>20</span>
-				</a>
-				<a href="javascript:void(0);" class="left">
-					<svg class="icon icon_em_25" aria-hidden="true">
-						<use xlink:href="#front_icon-icondianzan"></use>
-					</svg>
-					<span>14</span>
-				</a>
-				<div style="clear: both;"></div>
-			</div><!--fun_info_bar-->
-		</div><!--theme_wrap-->
+		@foreach($list as $v)
+			<div class="theme_wrap">
+				<a href="{{url("web/theme/details?id=$v->id&cate_id=$v->cate_id")}}" class="theme_img_bar">
+					<img src='{{asset("upload/images/$v->banner_img")}}'  />
+					<div class="theme_cate_tip">{{$v->cate_name}}</div><!--theme_cate_tip-->
+				</a><!--theme_bar-->
+				<a href="{{url("web/theme/details?id=$v->id&cate_id=$v->cate_id")}}" class="title_h2">{{$v->title}}</a>
+				<div class="fun_info_bar">
+					<span class="left">
+						<svg class="icon icon_em_25" aria-hidden="true">
+	                        <use xlink:href="#front_icon-pinglun"></use>
+	                   </svg>
+						@foreach($count as $a)
+							@if($v->id == $a->th_id)
+								<span>{{$a->num == 0 ? 0 : $a->num}}</span>
+							@endif
+						@endforeach
+					</span>
+					<span class="left">
+						<svg class="icon icon_em_25" aria-hidden="true">
+	                        <use xlink:href="#front_icon-liulan1"></use>
+	                    </svg>
+	                    <span>{{$v->good_num == 0 ? 0 : $v->good_num}}</span>
+                   	</span>
+					<div style="clear: both;"></div>
+				</div><!--fun_info_bar-->
+			</div><!--theme_wrap-->
+		@endforeach
 
-		<div class="theme_wrap">
-			<a href="./theme/details.html" class="theme_img_bar">
-				<img src="{{asset('images/web/theme_img.jpg')}}"  />
-				<div class="theme_cate_tip">美妆</div><!--theme_cate_tip-->
-			</a><!--theme_bar-->
-			<a href="./theme/details.html" class="title_h2">恋恋粉色季 画个甜甜的妆容吧！</a>
-			<div class="fun_info_bar">
-				<a href="javascript:void(0);" class="left">
-					<svg class="icon icon_em_25" aria-hidden="true">
-						<use xlink:href="#front_icon-pinglun"></use>
-				   </svg>
-					<span>20</span>
-				</a>
-				<a href="javascript:void(0);" class="left">
-					<svg class="icon icon_em_25" aria-hidden="true">
-						<use xlink:href="#front_icon-icondianzan"></use>
-					</svg>
-					<span>14</span>
-				</a>
-				<div style="clear: both;"></div>
-			</div><!--fun_info_bar-->
-		</div><!--theme_wrap-->
 	</div><!--wrap-->
 @endsection
